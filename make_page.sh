02045735a8320546b0023cd2b1a568bc11283188
Maciej Wojdak <48714815+maciej99@users.noreply.github.com>
2019-03-22 10:37:04 +0100

    Update make_page.sh

--- make_page.sh
+++ make_page.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# ------------------------------------------------------------
+# ---------------------------------------------------------
 # This script reads source files from an Android Project,
-# and creates a HTML page
+# and creates a HTML page.
 # Prerequisites:
 # - you need command "tree" installed
 # - other files of this set should be in the same folder:
-# color_java.sh, color_xml.sh, toc.html, index.html, style.css 
-# ------------------------------------------------------------
+# 	color_java.sh, color_xml.sh, toc.html,
+# 	index.html, style.css 
+# ---------------------------------------------------------
 usage() {
-  echo "Usage: `basename $0` [path_to_android_project]"
+	s=$(basename $0)
+	echo "Usage: $s [path_to_android_project]"
 }
 # validate argument: cannot be empty, nor non-existing path 
 if [ -z "$1" ]; then
@@ -20,24 +22,16 @@ elif [ ! -d "$1" ]; then
 	usage
 	exit 1
 fi
-# check other components...
-for f in color_java.sh color_xml.sh index.html style.css toc.html
-do
-	if [ ! -f "$f" ]; then
-		echo "File $f is missing."; exit 1;
-	fi
-done
-# make sure that argument path is an absolute path
-# it also cannot have a slash at the end
+# argument path must be an absolute path, with no slash tail
 cd $1
 PROJECTDIR=$(pwd)
 cd $OLDPWD
 # extract project name and set SRCDIR path
 PROJECTNAME=$(echo "$PROJECTDIR"  | sed -e 's/\/..*\//''/')
 SRCDIR="$PROJECTDIR/app/src/main"
-# if SRCDIR desn't exist - probably not an Android Project
+# if SRCDIR doesn't exist - probably not an Android Project
 if [ ! -d "$SRCDIR" ]; then		
-	echo -n "This path doesn't contain source directory"
+	echo -n "This path doesn't contain src directory"
 	echo -n " $SRCDIR "
 	echo "(Is it Android Project?)";
 	usage;
@@ -45,13 +39,40 @@ if [ ! -d "$SRCDIR" ]; then
 fi
 
 fileheading() {
-  # crop the full path, so it starts with [ProjectName].
-  ref=$(echo $1 | sed -e 's/^\/.*\/\('"$PROJECTNAME"'\)/\1/');
-  echo "<p id=\"$ref\">"
-  echo "<br>"
-  echo "File: <span class=\"filepath\">$ref</span>:"
-  echo "</p>"
+# crop the full path, so it starts with [ProjectName].
+ref=$(echo $1 | sed -e 's/^\/.*\/\('"$PROJECTNAME"'\)/\1/');
+echo "<p id=\"$ref\">"
+echo "<br>"
+echo "File: <span class=\"filepath\">$ref</span>:"
+echo "</p>"
 }
+contentshead() {
+# heading of contents div; use Projectname as an arg 
+echo "<!--begin:$1-->"
+echo "<div class=\"container\"id=\"heading_$1\">"
+echo -n "<div class=\"heading\" "
+echo -n "onclick=\"show('$1','heading_$1')\">"
+echo "$PROJECTNAME</div>"
+echo "<div class=\"hidenDiv\" id=\"$1\">"
+echo "<p class=\"hidenPar\">"
+echo "<div class=\"contents\">"
+echo -n "<a href=\"$1.html#top\" "
+echo "target=\"main\">$1 (page top)</a><br>"
+}
+contentstail() {
+# end of contents part; use Projectname as an arg
+echo -e "</pre>\n</div>\n</p>\n</div>\n</div>\n<!--end:$1-->"
+}
+pagehead() {
+# heading of project page; use Projectname as an arg
+echo '<html>'; echo '<head>'
+echo -n '<meta http-equiv="Content-Type" content="text/html"'
+echo 'charset="utf-8">'
+echo -n '<link rel="stylesheet" type="text/css"'
+echo ' href="style.css">'
+echo -e "<title>"$1"</title>\n</head>\n<body>\n<h1>"$1"</h1>" 
+}
+
 # ---- CREATING A HTML PAGE: [ProjectName].html ---
 page=$PROJECTNAME'.html';
 # check if such page already exists
@@ -69,41 +90,37 @@ fi
 echo "Reading files..."
 # Writing output of "tree" command to temporary file.
 # Note: for directories drawable- and mipmap- we will omit
-# files from directories with with
-# -ldpi, -mdpi, -hdpi, -xhdpi, -xxhdpi
+# files from directories with with -ldpi, -mdpi, -hdpi, etc.
 # to avoid listing files of the same names multiple times.
-# These files won't be included on the page.
-# From the tree listing, we also remove empty lines,
-# and the line with directories and files count.
+# Removing also empty lines and tree files info.
 dir_regex='\(mipmap\|drawable\)-x\{0,2\}[lmh]dpi'
-tree $SRCDIR -f | sed -e '/\/'"$dir_regex"'\/.*$/d; s/\/'"$dir_regex"'$/& [omitted]/; /^$/d; /^[0-9]\+ directories/d;' > tmp_tree;
+tree $SRCDIR -f |
+sed -e '/\/'"$dir_regex"'\/.*$/d;' \
+	-e 's/\/'"$dir_regex"'$/& [omitted]/;' \
+	-e '/^$/d;' \
+	-e '/^[0-9]\+ directories/d;' > tmp_tree;
 
 # write the beginig of contents block
-echo "<!--begin:$PROJECTNAME-->" > tmp_contents;
-echo "<div class=\"container\" id=\"heading_$PROJECTNAME\">" >> tmp_contents;
-echo "<div class=\"heading\" onclick=\"show('$PROJECTNAME','heading_$PROJECTNAME')\">$PROJECTNAME</div>" >> tmp_contents
-echo "<div class=\"hidenDiv\" id=\"$PROJECTNAME\">" >> tmp_contents
-echo "<p class=\"hidenPar\">" >> tmp_contents
-echo "<div class=\"contents\">" >> tmp_contents
-echo "<a href=\"$PROJECTNAME.html#top\" target=\"main\">$PROJECTNAME (page top)</a><br>" >> tmp_contents;
-# Changing some tree filenames into links <a href=...>.
-# As references we crop the full paths,
-# so they start with [ProjectName].
-# (they will be shorter, but still unique in the project)
-# The second sed command removes fullpath notation
-# from all the other lines (not converted to links).
-lead="<a href=\"$PROJECTNAME.html#";
-mid="\" target=\"main\">";
-tail="<\/a>";
+contentshead $PROJECTNAME > tmp_contents
+# changing some tree filenames into links <a href=...>.
+r1="\/.*\/\($PROJECTNAME\/.*\/\)\([a-zA-Z0-9_]*\.\)"
+r2="\(java\|kt\|xml\)$"
+reg=$r1""$r2
+# parts of link: lead, middle, tail
+L="<a href=\"$PROJECTNAME.html#";
+M="\" target=\"main\">";
+T="<\/a>";
 echo "<pre class=\"tree\">" >> tmp_contents;
-sed -e 's/\/.*\/\('"$PROJECTNAME"'\/.*\/\)\([a-zA-Z0-9_]*\.\)\(java\|kt\|xml\)$/'"$lead"'\1\2\3'"$mid"'\2\3'"$tail"'/' \
-	-e '/'"$lead"'/ !{s/\/.*\///;}' <tmp_tree >> tmp_contents;
+sed -e 's/'"$reg"'/'"$L"'\1\2\3'"$M"'\2\3'"$T"'/' \
+	-e '/'"$L"'/ !{s/\/.*\///;}' <tmp_tree >> tmp_contents;
 # write the end of contents block
-echo -e "</pre>\n</div>\n</p>\n</div>\n</div>\n<!--end:$PROJECTNAME-->" >> tmp_contents;
+contentstail $PROJECTNAME >> tmp_contents;
 
-# write the beginig of [ProjectName].html
-echo -e "<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html\" charset=\"utf-8\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
-<title>"$PROJECTNAME"</title>\n</head>\n<body>\n<h1>"$PROJECTNAME"</h1>" > $page;
+# write the begining of [ProjectName].html
+pagehead $PROJECTNAME > $page;
+# minimalist sed command to put src text into <pre>...<pre>
+preregex='s/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;'
+# read files as listed in contents
 cat tmp_tree | while read line;
 do
   f=$(echo $line | sed -e 's/^[^\/]*\(\/.*\)$/\1/');
@@ -115,7 +132,7 @@ do
       kt)
 	fileheading $f >> $page;
 	echo "<pre class=\"kotlin\">" >> $page;
-	sed -e 's/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;' $f >>$page
+	sed -e "$preregex" $f >>$page
 	echo "</pre>" >> $page;;
       xml)
 	fileheading $f >> $page;
@@ -126,16 +143,16 @@ done;
 echo -e "</body>\n</html>" >> $page;
 echo "Page $page has been created."; 
 
-# ---- INSERT THE CONTENTS BLOCK ----
+# ---- inserting the contents block into toc.html ----
 # if the script is run to overwrite existing page,
 # remove also old contents block, the new one goes at the top
 if [ "$overwriteflag" == "Y" ]; then
-	sed -i '/'"<\!--begin:$PROJECTNAME-->"'/,/'"<\!--end:$PROJECTNAME-->"'/{d}' toc.html
+	s1="<\!--begin:$PROJECTNAME-->"
+	s2="<\!--end:$PROJECTNAME-->"
+	sed -i '/'"$s1"'/,/'"$s2"'/{d}' toc.html
 fi
-# insert contents block into toc.html
-# below a marker <!--NEXT_PROJECT_HERE-->
-# note: sed with -i option writes changes directly to the file
-sed -i '/<\!--NEXT_PROJECT_HERE-->/ {
+# insert contents below marker <!--NEXT_PROJECT-->
+sed -i '/<\!--NEXT_PROJECT-->/ {
 	r tmp_contents
 }' toc.html
 # cleanup

7a11006961a2e1576508a58a9f63f5f82c399e2a
Maciej Wojdak <48714815+maciej99@users.noreply.github.com>
2019-03-22 10:32:23 +0100

    Update color_xml.sh

--- color_xml.sh
+++ color_xml.sh
@@ -1,45 +1,49 @@
 #!/bin/sh
-# Format XML file as a <pre>..</pre> block for html page.
-# Apply CSS style tags for color coding.
+# Format XML file as a <pre>...</pre> part of html page.
 if [ -z "$1" ]; then
-	echo `basename $0`" : Missing argument [file.xml]"
+	s=$(basename $0)
+	echo "$s : Missing argument [file.xml]"
 	exit 1 
 elif [ ! -f "$1" ]; then
-	echo `basename $0`" : File \"$1\" not found."
+	s=$(basename $0)
+	echo "$s : File \"$1\" not found."
 	exit 1
 fi
-# tags referring to CSS styles
-TAGAND="<span class=\"xml_android\">"
-TAGANDATR="<span class=\"xml_android_attr\">"
-TAGL="<span class=\"xml_string_literal\">"
-TAGBC="<span class=\"xml_block_comment\">"
-TAGSTOP='<\/span>'
+# tags for formatting pattern android:attr=
+T1="<span class=\"xml_android\">android<END>:"
+T2="<span class=\"xml_android_attr\">"
+# write output
 echo "<pre class=\"xml\">";
-sed -e 's/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;' < $1 | awk '
-BEGIN{mode=0;FS="";OFS="";}
+sed -e 's/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;' < $1 |
+awk 'BEGIN{mode=0;FS="";OFS="";}
 # mode: 0-xml code, 1-string literal 2-block comment
-# mask text inside by inserting "r>x"
+# mask text inside by inserting "x>x"
 {
 for(i=1; i<=NF; i++){ switch (mode) {
 	case "0":
-		# reading regular xml
-		# look for comments and string literals
-		if($i=="&" && $(i+1)=="l" && $(i+2)=="t" && $(i+3)==";" && $(i+4)=="!" && $(i+5)=="-" && $(i+6)=="-")
+		# looking for comments and string literals
+		txt=$i$(i+1)$(i+2)$(i+3)$(i+4)$(i+5)$(i+6)
+		if(txt=="&lt;!--") 
 			{$i="<BLK>&";i=i+6;mode=2}
 		else if($i=="\"") {$i="<LIT>\"";mode=1}			
 	        break
 	case "1":
 		if($i=="\"") {$i="\"<END>";mode=0}
-		else {gsub(/[a-z ]/,"&r>x",$i)};
+		else {gsub(/[a-z ]/,"&x>x",$i)};
 		break
 	case "2":
-		if($i=="-" && $(i+1)=="-" && $(i+2)=="&" && $(i+3)=="g" && $(i+4)=="t" && $(i+5)==";")
+		txt=$i$(i+1)$(i+2)$(i+3)$(i+4)$(i+5)
+		if(txt=="--&gt;")
 			{$(i+5)=";<END>";i=i+5;mode=0}
-		else {gsub(/[a-z ]/,"&r>x",$i)};
+		else {gsub(/[a-z ]/,"&x>x",$i)};
 		break			
 	default: mode=0; break
 	}
 } print $0;
-}' | sed -e 's/android\:\(.*\)\=/'"$TAGAND"'android<END>\:'"$TAGANDATR"'\1<END>\=/;' \
-	-e 's/<LIT>/'"$TAGL"'/g; s/<OLC>/'"$TAGC"'/g; s/<BLK>/'"$TAGBC"'/g; s/<END>/'"$TAGSTOP"'/g; s/r>x//g;';
+}' |
+sed -e 's/android\:\(.*\)\=/'"$T1""$T2"'\1<END>\=/;' \
+	-e 's/<LIT>/<span class=\"xml_string_literal\">/g;' \
+	-e 's/<BLK>/<span class=\"xml_block_comment\">/g;' \
+	-e 's/<END>/<\/span>/g;' \
+	-e 's/x>x//g;';
 echo "</pre>"

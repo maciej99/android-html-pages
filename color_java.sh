035fc8ce39cf31d63878b2009a93d5dc71f6dd11
Maciej Wojdak <48714815+maciej99@users.noreply.github.com>
2019-03-22 10:30:25 +0100

    Update color_java.sh

--- color_java.sh
+++ color_java.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
-# Format JAVA file as a <pre>..</pre> block for html page.
-# Apply CSS style tags for color coding.
+# Format Java file as a <pre>...</pre> part of html page.
 if [ -z "$1" ]; then
-	echo `basename $0`" : Missing argument [file.java]"
+	s=$(basename $0)
+	echo "$s : Missing argument [file.java]"
 	exit 1
 elif [ ! -f "$1" ]; then
-	echo `basename $0`" : File \"$1\" not found."
+	s=$(basename $0)
+	echo "$s : File \"$1\" not found."
 	exit 1
 fi
-# tags for colors referring to CSS styles
+# css tag and regex for java keywords
 TAGJ="<span class=\"java_keyword\">"
-TAGC="<span class=\"java_comment\">"
-TAGL="<span class=\"java_string_literal\">"
-TAGBC="<span class=\"java_block_comment\">"
-TAGSTOP='<\/span>'
+j1="abstract\|assert\|boolean\|break\|byte\|case"
+j2="\|catch\|char\|class\|const\|continue\|default\|do"
+j3="\|double\|else\|enum\|extends\|final\|finally\|float"
+j4="\|for\|goto\|if\|implements\|import\|instanceof\|int"
+j5="\|interface\|long\|native\|new\|package\|private"
+j6="\|protected\|public\|return\|short\|static\|strictfp"
+j7="\|super\|switch\|synchronized\|this\|throw\|throws"
+j8="\|transient\|try\|void\|volatile\|while"
+jx=$j1""$j2""$j3""$j4""$j5""$j6""$j7""$j8
+jregex="\(^\|[^a-zA-Z0-9_]*\)\($jx\)\([^a-zA-Z0-9_]\)"
 
-java_regex="abstract\|assert\|boolean\|break\|byte\|case\|catch\|char\|class\|const\|continue\|default\|do\|double\|else\|enum\|extends\|final\|finally\|float\|for\|goto\|if\|implements\|import\|instanceof\|int\|interface\|long\|native\|new\|package\|private\|protected\|public\|return\|short\|static\|strictfp\|super\|switch\|synchronized\|this\|throw\|throws\|transient\|try\|void\|volatile\|while";
 echo "<pre class=\"java\">";
-sed -e 's/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;' $1 | awk '
-BEGIN{mode=0;FS="";OFS="";}
-# mode: 0-code, 1-literal, 2-one-line comment, 3-block comment
-# mask text inside comments and literals by inserting "r>b"
-# FS is empty, reading single chars
-# OFS output separator is also empty
+sed -e 's/&/\&amp;/g;s/</\&lt;/g;s/>/\&gt;/g;' $1 |
+awk 'BEGIN{mode=0;FS="";OFS="";}
+# mode:0-code,1-literal,2-one-line comment,3-block comment
+# mask text in comments and literals by inserting "x>x"
+# FS is empty, reading single chars, OFS empty too
 {
 for(i=1; i<=NF; i++){
 	switch (mode) {
@@ -35,22 +40,22 @@ for(i=1; i<=NF; i++){
 				{$i="<BLK>/";mode=3}
 		        break
 		case "1":
-			# reading literal, mask and look for "
+			# reading literal, mask, look for "
 			if($i=="\"")
 				{$i="\"<END>";mode=0}
-			else {gsub(/[a-z ]/,"&r>b",$i)};
+			else {gsub(/[a-z ]/,"&x>x",$i)};
 			break	    
 		case "2":
 			# reading one-line comment
 			# mask till the end of the line
-			gsub(/[a-z ]/,"&r>b",$i);
+			gsub(/[a-z ]/,"&x>x",$i);
 			break
 		case "3":
 			# reading block comment
 			# mask and look for */
 			if($i=="*" && $(i+1)=="/")
 				{$(i+1)="/<END>";mode=0}
-			else {gsub(/[a-z ]/,"&r>b",$i)};
+			else {gsub(/[a-z ]/,"&x>x",$i)};
 			break
 		default: mode=0; break
 		}
@@ -58,7 +63,12 @@ for(i=1; i<=NF; i++){
 	if(mode==2)
 		{print $0,"<END>"; mode=0;}
 	else {print $0};
-}' | sed -e 's/\(^\|[^a-zA-Z0-9_]*\)\('$java_regex'\)\([^a-zA-Z0-9_]\)/\1'"$TAGJ"'\2'"$TAGSTOP"'\3/g' \
-	-e 's/'"$TAGSTOP"'\([ ^I]\)'"$TAGJ"'/\1/g;' \
-	-e 's/<LIT>/'"$TAGL"'/g; s/<OLC>/'"$TAGC"'/g; s/<BLK>/'"$TAGBC"'/g; s/<END>/'"$TAGSTOP"'/g; s/r>b//g;';
+}' |
+sed -e 's/'"$jregex"'/\1'"$TAGJ"'\2<\/span>\3/g' \
+	-e 's/<\/span>\([ ^I]\)'"$TAGJ"'/\1/g;' \
+	-e 's/<LIT>/<span class=\"java_string_literal\">/g;' \
+	-e 's/<OLC>/<span class=\"java_comment\">/g;' \
+	-e 's/<BLK>/<span class=\"java_block_comment\">/g;' \
+	-e 's/<END>/<\/span>/g;' \
+	-e 's/x>x//g;';
 echo "</pre>";
